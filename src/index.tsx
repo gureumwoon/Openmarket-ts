635f62a9f318f8be9acf4adb54e178c0e2f658d6
gureumwoon <vfrvfr3221@gmail.com>
2023-05-09 15:03:44 +0900

    custom.d.ts 파일 추가해서 assets 이미지 파일들 import로 사용 가능

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,11 +8,13 @@ import store from "./redux/configStore";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
