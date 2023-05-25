43c4f623b3cc1449031eae8fed8ad3e507772fff
gureumwoon <vfrvfr3221@gmail.com>
2023-05-25 23:11:21 +0900

    Login, SignUp, Upload, ProductDetail routes 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { Routes, Route } from "react-router-dom";
 
 // routes
 import Main from './pages/Main';
+import Login from './pages/LogIn';
+import SignUp from './pages/SingUp';
+import Upload from './pages/Upload';
+import ProductDetail from './pages/ProductDetail';
 
 
 function App() {
@@ -13,7 +17,12 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Main />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/edit/:id" element={<Upload />} />
+        <Route path="/detail/:id" element={<ProductDetail />} />
       </Routes>
     </div>
   );
