48b15c21033a0b0c9d567d795b1f9d84a71c7829
gureumwoon <vfrvfr3221@gmail.com>
2023-05-30 23:30:12 +0900

    ProductDetail interface 추가

--- src/components/types/product.ts
+++ src/components/types/product.ts
@@ -1,34 +1,70 @@
-export interface UploadProduct {
-    product_name: string,
-    image: string,
-    price: number,
-    shipping_method: string,
-    shipping_fee: number,
-    stock: number,
-    products_info: string,
+export interface Product {
+    product_id: number;
+    product_name: string;
+    seller: number;
+    seller_store: string;
+    image: string;
+    price: number;
+    shipping_method: string;
+    shipping_fee: number;
+    stock: number;
+    products_info: string;
 }
 
-export interface ModifyProduct {
-    product_name: string,
-    image: string,
-    price: number,
-    shipping_method: string,
-    shipping_fee: number,
-    stock: number,
-    products_info: string,
+export interface ProductDetail {
+    created_at: string;
+    product_id: number;
+    updated_at: string;
+    product_name: string;
+    image: string;
+    price: number;
+    shipping_method: string;
+    shipping_fee: number;
+    stock: number;
+    products_info: string;
+    seller: number;
+    store_name: string;
 }
 
+// export interface UploadProduct {
+//     product_name: string,
+//     image: string,
+//     price: number,
+//     shipping_method: string,
+//     shipping_fee: number,
+//     stock: number,
+//     products_info: string,
+//     token: string
+// }
+
+// export interface ModifyProduct {
+//     product_name: string,
+//     image: string,
+//     price: number,
+//     shipping_method: string,
+//     shipping_fee: number,
+//     stock: number,
+//     products_info: string,
+// }
 
 // cart
 
 export interface AddCart {
-    product_id: number,
-    quantity: number,
-    check: boolean
+    product_id: number;
+    quantity: number;
+    check: boolean;
 }
 
 export interface ModifyCartQuantity {
-    product_id: number,
-    quantity: number,
-    is_active: boolean
+    product_id: number;
+    quantity: number;
+    is_active: boolean;
+}
+
+export interface CartDetail {
+    my_cart: number;
+    cart_item_id: number;
+    product_id: number;
+    quantity: number;
+    is_active?: boolean;
 }
