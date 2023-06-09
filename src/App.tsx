e0d542fa76d0c6eb1a7152e56862a0436462b773
gureumwoon <vfrvfr3221@gmail.com>
2023-06-09 23:22:28 +0900

    장바구니 page 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,12 @@ import Login from './pages/LogIn';
 import SignUp from './pages/SingUp';
 import Upload from './pages/Upload';
 import ProductDetail from './pages/ProductDetail';
-
+import ShoppingCart from './pages/ShoppingCart';
+import { useAppSelector } from './hooks/reduxHooks';
 
 function App() {
 
-  // const itemCount = useSelector((state) => state.product.count)
+  const itemCount = useAppSelector((state) => state.product.count)
 
   return (
     <div className="App">
@@ -23,6 +24,7 @@ function App() {
         <Route path="/upload" element={<Upload />} />
         <Route path="/edit/:id" element={<Upload />} />
         <Route path="/detail/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<ShoppingCart itemCount={itemCount} />} />
       </Routes>
     </div>
   );
