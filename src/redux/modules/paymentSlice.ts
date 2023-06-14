3ca624093476e8ab18f6b69c03315a9d4ce45ace
gureumwoon <vfrvfr3221@gmail.com>
2023-06-14 22:43:32 +0900

    PaymentSlice 작성 완료

---
+++ src/redux/modules/paymentSlice.ts
@@ -0,0 +1,33 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { apis } from "../../shared/api";
+
+const initialState = {
+    paymentList: [],
+}
+
+export const addPayment = createAsyncThunk(
+    "payment/addPayment",
+    async (item: PaymentItem) => {
+        try {
+            const res = await apis.directOrder(item)
+            return res.data
+        } catch (error) {
+            console.log("바로구매에러", error)
+            throw error;
+        }
+    }
+)
+
+const paymentSlice = createSlice({
+    name: 'payment',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(addPayment.fulfilled, (state, action) => {
+                state.paymentList = action.payload;
+            })
+    }
+})
+
+export default paymentSlice.reducer
