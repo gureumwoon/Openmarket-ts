b13da23bc0b99d63d68b151c61375f5ec875f0b0
gureumwoon <vfrvfr3221@gmail.com>
2023-06-17 00:11:36 +0900

    PostCode component props,data ts 선언 완료

---
+++ src/components/PostCode.tsx
@@ -0,0 +1,32 @@
+import DaumPostcode, { Search } from 'react-daum-postcode';
+
+interface postCodeModalProps {
+    onComplete: () => void;
+}
+
+const PostCodeModal = (props: postCodeModalProps) => {
+    const { onComplete } = props;
+
+    const handleSearch = (data: Search) => {
+        console.log(data)
+    }
+
+    return (
+        <>
+            <DaumPostcode
+                onComplete={onComplete}
+                onSearch={handleSearch}
+                autoClose
+            />
+        </>
+    )
+}
+
+PostCodeModal.defaultProps = {
+    style: {
+        width: "700px",
+        height: "450px",
+    },
+};
+
+export default PostCodeModal
