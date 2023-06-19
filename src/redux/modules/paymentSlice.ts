4dda4e4f9e8023e0c8ac130cb274b818a2da808c
gureumwoon <vfrvfr3221@gmail.com>
2023-06-19 20:22:39 +0900

    PaymentSlice에 PaymentItem import 해오기

--- src/redux/modules/paymentSlice.ts
+++ src/redux/modules/paymentSlice.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { apis } from "../../shared/api";
+import { PaymentItem } from "../../components/types/product";
 
 const initialState = {
     paymentList: [],
