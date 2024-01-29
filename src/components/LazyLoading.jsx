7158a8ae5a68bfba6fbf323be4fcf5096fc46509
gureumwoon <vfrvfr3221@gmail.com>
2024-01-30 01:59:07 +0900

    LazyImage loading시에는 placeholderImg보여주게 설정 완료

--- src/components/LazyLoading.jsx
+++ src/components/LazyLoading.jsx
@@ -2,8 +2,9 @@ import styled from "styled-components";
 import useInfiniteScroll from "../hooks/use-infinitescroll"
 import { useEffect, useState } from "react";
 
-export default function LazyLoadingImage({ src, alt, onError, onClick }) {
+export default function LazyLoadingImage({ src, alt, onError, onClick, placeholderImg }) {
     const [isLoading, setIsLoading] = useState(true);
+    const [imgSrc, setImgSrc] = useState(placeholderImg);
 
     const target = useInfiniteScroll(handleIntersection);
 
@@ -13,9 +14,11 @@ export default function LazyLoadingImage({ src, alt, onError, onClick }) {
         image.src = src;
         image.onload = () => {
             setIsLoading(false);
+            setImgSrc(src)
         };
         image.onerror = () => {
             setIsLoading(true);
+            setImgSrc(src)
         };
 
         return () => {
@@ -28,13 +31,14 @@ export default function LazyLoadingImage({ src, alt, onError, onClick }) {
         const entry = entries[0];
         if (entry.isIntersecting) {
             setIsLoading(false);
+            setImgSrc(src)
         }
     }
 
     return (
         <LazyImage
             className={isLoading ? 'loading' : 'loaded'}
-            src={src}
+            src={imgSrc}
             loading="lazy"
             alt={isLoading ? "" : alt}
             onError={onError || null}
@@ -50,6 +54,7 @@ export default function LazyLoadingImage({ src, alt, onError, onClick }) {
 const LazyImage = styled.img`
     display: block;
     width: 100%;
+    height: 100%;
     transition: all 0.5s;
 
   &.loading {
